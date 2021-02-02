145fb3ed333d486dc6a8f3920409d68456357eda
Rae Stichter <raestichter@gmail.com>
2021-02-01 23:59:53 -0500

    added functionality to join tables

--- develop/server.js
+++ develop/server.js
@@ -19,7 +19,11 @@ const db = new sqlite3.Database('./db/election.db', err => {
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err, rows) => {
       if (err) {
@@ -42,19 +46,24 @@ app.get('/api/candidates', (req, res) => {
 //     console.log(row);
 // });
 
-// Get all candidates
-app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
-    const params = [];
-    db.all(sql, params, (err, rows) => {
+// Get single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
       if (err) {
-        res.status(500).json({ error: err.message });
+        res.status(400).json({ error: err.message });
         return;
       }
   
       res.json({
         message: 'success',
-        data: rows
+        data: row
       });
     });
 });
